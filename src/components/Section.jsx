70cbe98448e486702b26cafa774b3ac010501490
Olivier Larose <olivierlarose1@gmail.com>
2024-05-25 10:04:08 -0400

    adjust typo

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -19,7 +19,7 @@ export default function Section() {
         >
         <div className='relative z-10 p-20 mix-blend-difference text-white w-full h-full flex flex-col justify-between'>
             <p className='w-[50vw] text-[2vw] self-end uppercase mix-blend-difference'>Beauty and quality need the right time to be conceived and realised even in a world that is in too much of a hurry.</p>
-            <p className='text-[7.5vw] uppercase mix-blend-difference'>Background Parallax</p>
+            <p className='text-[5vw] uppercase mix-blend-difference'>Background Parallax</p>
         </div>
         <div className='fixed top-[-10vh] left-0 h-[120vh] w-full'>
             <motion.div style={{y}} className='relative w-full h-full'>
